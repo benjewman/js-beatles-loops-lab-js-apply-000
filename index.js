ca2f7c597c362ce89b20273e04ef6a60beba8af2
Benjamin Davis <bennyadave@gmail.com>
2019-04-08 05:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return emptyArray
 }
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while(i < facts.length) {
+    facts[i] = facts[i] + '!!!'
+    i++
+  }
+  return facts
+}
